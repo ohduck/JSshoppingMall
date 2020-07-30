39920fcb1548663617fba0a5918cad4b27d8b8f2
ohduck <ohduck2@naver.com>
2020-07-30 11:25:24 +0900

    정렬기능 추가
    가격별, 가나다순 정렬기능 추가

--- homework/tab.js
+++ homework/tab.js
@@ -7,3 +7,23 @@ document.getElementById('1').addEventListener('click', function(){
     document.getElementById('content0').className = 'tab-content';
     document.getElementById('content2').className = 'tab-content';
 });
+
+document.getElementById('0').addEventListener('click', function(){
+    document.getElementById('0').className = 'tab-button orange';
+    document.getElementById('1').className = 'tab-button';
+    document.getElementById('2').className = 'tab-button';
+    
+    document.getElementById('content0').className = 'tab-content show';
+    document.getElementById('content1').className = 'tab-content';
+    document.getElementById('content2').className = 'tab-content';
+});
+
+document.getElementById('2').addEventListener('click', function(){
+    document.getElementById('2').className = 'tab-button orange';
+    document.getElementById('1').className = 'tab-button';
+    document.getElementById('0').className = 'tab-button';
+    
+    document.getElementById('content2').className = 'tab-content show';
+    document.getElementById('content1').className = 'tab-content';
+    document.getElementById('content0').className = 'tab-content';
+});
